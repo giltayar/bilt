b4e56843049f44b28a9dcb358d73f65c13e44f87
Gil Tayar <gil@tayar.org>
2017-07-15 10:04:47 +0300

    first package - mostly configuration stuff

---
+++ jobs/jobs.js
@@ -0,0 +1,9 @@
+"use strict"
+
+module.exports = ({ pluginRepository }) => {
+  return {
+    async runJob(job) {
+      const plugin = pluginRepository.findPlugin({ kind: "Job", jobKind: job.xxx })
+    }
+  }
+}
