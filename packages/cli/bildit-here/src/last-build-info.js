c9cab347fcf696aa0083b721b1e77dfbd7cb3513
Gil Tayar <gil@tayar.org>
2017-07-30 22:39:30 +0300

    commit-1

--- packages/cli/bildit-here/src/last-build-info.js
+++ packages/cli/bildit-here/src/last-build-info.js
@@ -86,7 +86,6 @@ async function readHashesOfFiles(directory, files) {
   const hashAndFiles = await Promise.all(
     files.map(async file => [path.relative(directory, file), await readHashOfFile(file)]),
   )
-  console.log('hasheandfiles', hashAndFiles)
 
   return hashAndFiles.reduce(
     (obj, [file, hash]) => Object.assign(obj, {[file]: hash}),
