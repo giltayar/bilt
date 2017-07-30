7526053480fa644fc1cdc9c7e4074d617977e7b1
Gil Tayar <gil@tayar.org>
2017-07-30 22:04:50 +0300

    commit-1

--- packages/cli/bildit-here/test/last-build-info.test.js
+++ packages/cli/bildit-here/test/last-build-info.test.js
@@ -4,12 +4,18 @@ const path = require('path')
 const replayGitRepo = require('./replay-git-repo')
 const lastBuildInfo = require('../src/last-build-info')
 
-describe('readLastBuildInfo', () => {
+describe('readLastBuildInfo and saveBuildInfo', () => {
   let gitDir
 
   beforeEach(async () => (gitDir = await replayGitRepo(path.join(__dirname, 'test-folder'))))
 
   it('should return undefined when no .bildit folder', async () => {
     expect(await lastBuildInfo.readLastBuildInfo(gitDir)).to.be.undefined
+  })
+
+  it('should enable saving and re-reading', async () => {
+    await lastBuildInfo.saveBuildInfo(gitDir, {something: 4})
+
+    expect(await lastBuildInfo.readLastBuildInfo(gitDir)).to.deep.equal({something: 4})
   })
 })
