95661b2a87fc3c1f4a294bb0627e38468a3b6c3d
Gil Tayar <gil@tayar.org>
2017-07-30 23:04:17 +0300

    commit-1

--- packages/cli/bildit-here/test/last-build-info.test.js
+++ packages/cli/bildit-here/test/last-build-info.test.js
@@ -32,11 +32,12 @@ describe('last-build-info', () => {
     })
 
     it('should show file changes in one file that we touch', async () => {
+      const lastBuildInfo = await buildInfo.findChangesInCurrentRepo(gitDir)
       await p(fs.writeFile)(path.join(gitDir, 'a.txt'), 'lalala')
 
       const changes = await buildInfo.findChangesInCurrentRepo(gitDir)
 
-      expect(changes.commit).to.be.ok
+      expect(changes.commit).to.equal(lastBuildInfo.commit)
       expect(changes.changedFilesInWorkspace).to.deep.equal({
         'a.txt': '9aa6e5f2256c17d2d430b100032b997c',
       })
@@ -65,13 +66,14 @@ describe('last-build-info', () => {
 
       const currentRepoInfo = await buildInfo.findChangesInCurrentRepo(gitDir)
 
-      const changedFiles = buildInfo.calculateChangesToBuildSinceLastBuild(
+      const changes = buildInfo.calculateChangesToBuildSinceLastBuild(
         gitDir,
         lastBuildInfo,
         currentRepoInfo,
       )
 
-      expect(changedFiles).to.have.members(['a.txt', 'c.txt'])
+      expect(changes.changedFilesThatNeedBuild).to.have.members(['a.txt', 'c.txt'])
+      expect(changes.fromCommit).to.equal(lastBuildInfo.commit)
     })
   })
 })
