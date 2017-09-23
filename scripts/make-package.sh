0a77be254bdddaaf73c9d4b3f99eb55b48dbd883
Gil Tayar <gil@tayar.org>
2017-09-23 09:18:46 +0300

    make-package.sh now renames template file names

--- scripts/make-package.sh
+++ scripts/make-package.sh
@@ -9,13 +9,13 @@ TARGET=packages/$PACKAGE_NAME
 pushd $SCRIPTDIR/..
 
 mkdir $TARGET
-cp -R -n $TEMPLATE $TARGET
+cp -R -n $TEMPLATE/* $TARGET
 
 for i in `find $TARGET -type f`; do
     sed -i '' "s/templatetemplate/$PACKAGE_NAME/g" "$i"
 done
 
-mv $TARGET/scripts/run-templatetemplate.js $TARGET/scripts/run-$PACKAGE_NAME.js
-mv $TARGET/src/templatetemplate.js $TARGET/src/$PACKAGE_NAME.js
+REPLACE_IN_FILES=`find $TARGET -name '*templatetemplate*' | awk '{str=$0;gsub("templatetemplate", "$PACKAGE_NAME", str); print "mv", $0, str}'`
+eval "$REPLACE_IN_FILES"
 
 popd
