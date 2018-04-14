0bfd910f340d0eaf182599246fd6196b0b9cce20
Gil Tayar <gil@tayar.org>
2018-04-14 10:38:22 -0700

    bc: started working on the command line options. defined the mvp

--- packages/cli/bilt-cli/scripts/run-bilt-cli.js
+++ packages/cli/bilt-cli/scripts/run-bilt-cli.js
@@ -1,3 +1,5 @@
+'use strict'
+const yargs = require('yargs')
 const biltHere = require('../src/bilt-cli')
 
 process.on('unhandledRejection', err => {
@@ -5,6 +7,35 @@ process.on('unhandledRejection', err => {
   process.exit(2)
 })
 
-const args = process.argv.slice(2)
+const argv = yargs
+  .env('BILT_OPTIONS')
+  .version()
+  .option('build', {
+    alias: 'b',
+    description: 'directory or artifact name to build. It will build just this artifact',
+    array: true,
+    default: '.',
+  })
+  .option('root', {
+    alias: 'r',
+    description: 'directory or artifact name to build, including its dependencies and dependees',
+    array: true,
+  })
+  .option('upto', {
+    alias: 'u',
+    description: 'directory or artifact name to build, including its dependees',
+    array: true,
+  })
+  .options('force', {
+    alias: 'f',
+    description: 'force builds, whether it is needed or not',
+    boolean: true,
+    default: false,
+  })
+  .options('execute', {
+    alias: '-',
+    description: 'executes the following command on the artifacts instead of building them',
+    array: true,
+  })
 
 biltHere(args[0], args[1]).catch(err => console.log(err.stack || err))
