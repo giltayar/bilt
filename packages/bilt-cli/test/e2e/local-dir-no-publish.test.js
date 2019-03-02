91629e8e33556f00e1aa35cdfde3b56ee8df1968
f <f@f.com>
2019-03-02 09:29:04 +0200

    bc: fixed so it can run in CI too

--- packages/bilt-cli/test/e2e/local-dir-no-publish.test.js
+++ packages/bilt-cli/test/e2e/local-dir-no-publish.test.js
@@ -15,16 +15,14 @@ describe('local directory use-case (e2e)', () => {
   it('should build the directory with all its packages and then say there is nothing to rebuild', async () => {
     const buildDir = await setupBuildDir(testRepoSrc)
 
-    await p(exec)(`${process.argv0} ${cli} ${buildDir} -d publish -d increment-version`)
+    console.log(await p(exec)(`${process.argv0} ${cli} ${buildDir} --no-ci`))
 
     expect(await fileContents(buildDir, 'a/postinstalled.txt')).to.equal('')
     expect(await fileContents(buildDir, 'b/postinstalled.txt')).to.equal('')
     expect(await fileContents(buildDir, 'b/built.txt')).to.equal('')
     expect(await fileContents(buildDir, 'b/tested.txt')).to.equal('')
 
-    const {stdout} = await p(exec)(
-      `${process.argv0} ${cli} ${buildDir} -d publish -d increment-version`,
-    )
+    const {stdout} = await p(exec)(`${process.argv0} ${cli} ${buildDir} --no-ci`)
     expect(stdout).to.contain('Nothing to build')
   })
 })
