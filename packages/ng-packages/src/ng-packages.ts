e6511b4cd724d8ce9eb1d65cb4fcbcd38745641d
Gil Tayar <gil@tayar.org>
2020-02-15 20:55:28 +0200

    np: first publish

---
+++ packages/ng-packages/src/ng-packages.ts
@@ -0,0 +1,92 @@
+import {promises as fs} from 'fs'
+import makeFindArtifacts from '@bilt/artifact-finder'
+
+export interface Directory extends String {}
+export interface RelativeDirectoryPath extends String {}
+export interface RelativeFilePath extends String {}
+export interface Commitish extends String {}
+
+export interface Package {
+  directory: RelativeDirectoryPath
+}
+
+export interface PackageInfo extends Package {
+  name: string
+  dependencies: Package[]
+}
+
+export interface PackageInfos {
+  [packageDirectory: string]: PackageInfo
+}
+
+export async function findNpmPackages({
+  rootDirectory,
+}: {
+  rootDirectory: Directory
+}): Promise<Package[]> {
+  const {findArtifacts} = await makeFindArtifacts()
+
+  const result = (await findArtifacts(rootDirectory)) as {path: string}[]
+
+  return result.map(artifact => ({directory: artifact.path}))
+}
+
+export async function findNpmPackageInfos({
+  packages,
+}: {
+  packages: Package[]
+}): Promise<PackageInfos> {
+  const interimPackageInfos = await Promise.all(packages.map(pkg => loadInterimPackageInfo(pkg)))
+
+  const packageNameToPackagePath = Object.fromEntries(
+    interimPackageInfos.map(interimPackageInfo => [
+      interimPackageInfo.name,
+      interimPackageInfo.directory,
+    ]),
+  )
+
+  return Object.fromEntries(
+    interimPackageInfos.map(interimPackageInfo => [
+      interimPackageInfo.directory,
+      interimPackageInfoToPackageInfo(interimPackageInfo, packageNameToPackagePath),
+    ]),
+  )
+}
+
+type InterimPackageInfo = {
+  directory: RelativeDirectoryPath
+  name: string
+  dependencies: string[]
+}
+
+async function loadInterimPackageInfo(pkg: Package): Promise<InterimPackageInfo> {
+  const packageJson = JSON.parse(await fs.readFile(pkg.directory as string, 'utf-8'))
+  const name = packageJson.name
+  const dependenciesByName = [
+    ...Object.keys(packageJson.dependencies || []),
+    ...Object.keys(packageJson.devDependencies || []),
+  ]
+
+  return {
+    ...pkg,
+    name,
+    dependencies: dependenciesByName,
+  }
+}
+
+function interimPackageInfoToPackageInfo(
+  interimPackageInfo: InterimPackageInfo,
+  packageNamesToPackagePaths: {[packageName: string]: RelativeDirectoryPath},
+): PackageInfo {
+  return {
+    directory: interimPackageInfo.directory,
+    name: interimPackageInfo.name,
+    dependencies: (interimPackageInfo.dependencies
+      .map(dep =>
+        packageNamesToPackagePaths[dep] == null
+          ? {package: packageNamesToPackagePaths[dep]}
+          : undefined,
+      )
+      .filter(dep => dep !== undefined) as unknown) as PackageInfo[],
+  }
+}
---
+++ packages/ng-packages/test/unit/ng-build.unit.test.ts
@@ -0,0 +1,10 @@
+import {describe, it} from 'mocha'
+import {expect} from 'chai'
+
+import * as ng from '../../src/ng-packages'
+
+describe('ng-packages (unit)', function() {
+  it('should be able to add', async () => {
+    expect(ng).to.not.be.undefined
+  })
+})
