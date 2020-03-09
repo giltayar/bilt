9cec8278186c7ee509d72fd9c31bc2f23b1218e1
Gil Tayar <gil@tayar.org>
2020-03-09 21:53:19 +0200

    ne2e: started writing build scripts

--- packages/ng-e2e/src/ng-e2e.ts
+++ packages/ng-e2e/src/ng-e2e.ts
@@ -1,9 +1,96 @@
-export interface Add {
-  a: number
-  b: number
-}
-export function add({a, b}: Add) {
-  const ret = a + b
+import path from 'path'
+import {promisify} from 'util'
+import {exec} from 'child_process'
+import {
+  Directory,
+  findNpmPackageInfos,
+  findNpmPackages,
+  RelativeDirectoryPath,
+} from '@bilt/ng-packages'
+import {
+  findChangedFiles,
+  findChangedPackages,
+  calculatePackagesToBuild,
+} from '@bilt/ng-what-to-build'
+import {
+  loadCommitsOfLastSuccesfulBuilds,
+  calculateBuildOrder,
+  build,
+  BuildPackageFunction,
+  saveCommitOfLastSuccesfulBuild,
+} from '@bilt/ng-build'
 
-  return ret
+export async function buildJustChangedPackages(
+  rootDirectory: Directory,
+  buildPackageFunc: BuildPackageFunction,
+) {
+  const {changedPackages, packageInfos, commit} = await commonBuild(rootDirectory)
+
+  const packagesToBuild = calculatePackagesToBuild({
+    packageInfos,
+    basePackagesToBuild: changedPackages,
+    buildUpTo: [],
+  })
+
+  const buildOrder = calculateBuildOrder({packageInfos: packagesToBuild})
+
+  for await (const buildPackageResult of build({packageInfos, buildOrder, buildPackageFunc})) {
+    console.log(
+      `package ${buildPackageResult.package} built: ${buildPackageResult.buildResult}${
+        buildPackageResult.error ? '. Error: ' + buildPackageResult.error : ''
+      }`,
+    )
+    await saveCommitOfLastSuccesfulBuild({rootDirectory, buildPackageResult, commit})
+  }
+}
+
+export async function buildUpTo(
+  rootDirectory: Directory,
+  upToPackages: RelativeDirectoryPath[],
+  buildPackageFunc: BuildPackageFunction,
+) {
+  const {changedPackages, packageInfos, commit} = await commonBuild(rootDirectory)
+
+  const packagesToBuild = calculatePackagesToBuild({
+    packageInfos,
+    basePackagesToBuild: changedPackages,
+    buildUpTo: upToPackages.map(upTo => ({directory: upTo})),
+  })
+
+  const buildOrder = calculateBuildOrder({packageInfos: packagesToBuild})
+
+  for await (const buildPackageResult of build({packageInfos, buildOrder, buildPackageFunc})) {
+    console.log(
+      `package ${buildPackageResult.package} built: ${buildPackageResult.buildResult}${
+        buildPackageResult.error ? '. Error: ' + buildPackageResult.error : ''
+      }`,
+    )
+    await saveCommitOfLastSuccesfulBuild({rootDirectory, buildPackageResult, commit})
+  }
+}
+
+async function commonBuild(rootDirectory: Directory) {
+  const {stdout} = await promisify(exec)('git rev-parse HEAD')
+  const toCommit = stdout.trim()
+
+  const packages = await findNpmPackages({rootDirectory})
+  const packageInfos = await findNpmPackageInfos({rootDirectory, packages})
+  const packageSuccessfulCommits = await loadCommitsOfLastSuccesfulBuilds({
+    rootDirectory: path.join(rootDirectory as string, '.bilt'),
+    packages,
+  })
+
+  const changedFiles = (
+    await Promise.all(
+      packageSuccessfulCommits.map(packageSuccessfulCommit =>
+        packageSuccessfulCommit?.commit
+          ? findChangedFiles({rootDirectory, fromCommit: packageSuccessfulCommit.commit, toCommit})
+          : undefined,
+      ),
+    )
+  ).flat()
+
+  const changedPackages = await findChangedPackages({changedFiles, packages})
+
+  return {packageInfos, changedPackages, commit: toCommit}
 }
