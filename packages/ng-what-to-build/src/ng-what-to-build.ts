1a3487f80e686692234565ab00dcb435a97ac15f
Gil Tayar <gil@tayar.org>
2020-03-27 12:30:29 +0300

    nwtb: implement new interface for git stuff

--- packages/ng-what-to-build/src/ng-what-to-build.ts
+++ packages/ng-what-to-build/src/ng-what-to-build.ts
@@ -8,40 +8,107 @@ import {
   RelativeFilePath,
   Directory,
   PackageInfo,
+  RelativeDirectoryPath,
 } from '@bilt/ng-packages'
+import {lstatSync} from 'fs'
+
+export type ChangedFilesInGit = Map<Commitish, RelativeFilePath[]>
 
 export async function findChangedFiles({
   rootDirectory,
-  fromCommit,
-  toCommit,
+  fromGitDate = '1 year ago',
+  toCommit = 'HEAD',
 }: {
-  rootDirectory: Directory
-  fromCommit: Commitish
+  fromGitDate: string
   toCommit: Commitish
-}): Promise<RelativeFilePath[]> {
+  rootDirectory: Directory
+}): Promise<ChangedFilesInGit> {
+  const COMMIT_PREFIX_IN_LOG = '----'
   const diffTreeResult = await promisify(execFile)(
     'git',
-    ['diff-tree', '--no-commit-id', '--name-only', '-r', fromCommit as string, toCommit as string],
+    [
+      'log',
+      `--format=format:${COMMIT_PREFIX_IN_LOG}%H`,
+      '--name-only',
+      `--since="${fromGitDate}"`,
+      toCommit as string,
+    ],
     {
       cwd: rootDirectory as string,
     },
   )
+  const gitLogLines = diffTreeResult.stdout
+    .split('\n')
+    .map(l => l.trim())
+    .filter(l => !!l)
 
-  const changedFiles = diffTreeResult.stdout.split('\n').filter(l => !!l)
+  const ret = new Map<Commitish, RelativeFilePath[]>()
 
-  return [...new Set(changedFiles)]
+  for (const gitLogLine of gitLogLines) {
+    var currentCommit //eslint-disable-line
+    if (gitLogLine.startsWith(COMMIT_PREFIX_IN_LOG)) {
+      currentCommit = gitLogLine.trim().slice(COMMIT_PREFIX_IN_LOG.length)
+      ret.set(currentCommit, [])
+    } else if (currentCommit) {
+      ;(ret.get(currentCommit) as RelativeFilePath[]).push(gitLogLine)
+    } else {
+      throw new Error(`something is wrong here: ${gitLogLine}`)
+    }
+  }
+
+  return ret
 }
 
+interface LastSuccesfulBuildOfPackage {
+  package: Package
+  lastSuccesfulBuild: Commitish
+}
+
+type LastSuccesfulBuildOfPackages = []
+
 export function findChangedPackages({
-  changedFiles,
-  packages,
+  changedFilesInGit,
+  lastSuccesfulBuildOfPackages,
 }: {
-  changedFiles: RelativeFilePath[]
-  packages: Package[]
+  changedFilesInGit: ChangedFilesInGit
+  lastSuccesfulBuildOfPackages: LastSuccesfulBuildOfPackage[]
 }): Package[] {
-  return packages.filter(pkg =>
-    changedFiles.some(changedFile => changedFile.startsWith(pkg.directory + '/')),
+  const packageChangeCounts = new Map<RelativeDirectoryPath, number>()
+  const lastSuccesfulBuildCommitToPackages: Map<Commitish, Package[]> = makeCommitsToPackages(
+    lastSuccesfulBuildOfPackages,
   )
+
+  const packages = lastSuccesfulBuildOfPackages.map(({package: pkg}) => pkg)
+
+  const packagesWhosLastSuccesfulBuildWasFound = new Set<RelativeDirectoryPath>()
+  let packageWithSuccefulBuildWasFound = false
+  for (const [commit, changedFiles] of [...changedFilesInGit.entries()].reverse()) {
+    packageWithSuccefulBuildWasFound =
+      packageWithSuccefulBuildWasFound || lastSuccesfulBuildCommitToPackages.has(commit)
+    if (!packageWithSuccefulBuildWasFound) continue
+
+    const packagesInCommit = packages.filter(pkg =>
+      changedFiles.some(changedFile => changedFile.startsWith(pkg.directory + '/')),
+    )
+    const packagesThatWereLastSuccesfullyBuiltInThisCommit = lastSuccesfulBuildCommitToPackages.get(
+      commit,
+    )
+
+    for (const packageInCommit of packagesInCommit) {
+      if (packagesThatWereLastSuccesfullyBuiltInThisCommit?.includes(packageInCommit)) {
+        packagesWhosLastSuccesfulBuildWasFound.add(packageInCommit.directory)
+      }
+      if (packagesWhosLastSuccesfulBuildWasFound.has(packageInCommit.directory))
+        packageChangeCounts.set(
+          packageInCommit.directory,
+          (packageChangeCounts.get(packageInCommit.directory) || 0) + 1,
+        )
+    }
+  }
+
+  return [...packageChangeCounts.entries()]
+    .filter(([, count]) => count > 1)
+    .map(([packageDirectory]) => ({directory: packageDirectory}))
 }
 
 export function calculatePackagesToBuild({
@@ -93,6 +160,24 @@ function artifactsToPackageInfos(
 
   for (const name of Object.keys(artifactsToBuild)) {
     ret[name] = packageInfos[name]
+  }
+
+  return ret
+}
+
+function makeCommitsToPackages(
+  lastSuccesfulBuildOfPackages: LastSuccesfulBuildOfPackage[],
+): Map<Commitish, Package[]> {
+  const ret = new Map<Commitish, Package[]>()
+
+  for (const {package: pkg, lastSuccesfulBuild: commit} of lastSuccesfulBuildOfPackages) {
+    const packages = ret.get(commit)
+
+    if (packages) {
+      packages.push(pkg)
+    } else {
+      ret.set(commit, [pkg])
+    }
   }
 
   return ret
