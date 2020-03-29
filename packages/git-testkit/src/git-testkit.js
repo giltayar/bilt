4eb6742b1ca7758f14faedcb2d0018778f568cb3
Gil Tayar <gil@tayar.org>
2020-03-29 22:19:35 +0300

    gt: first commit. works

---
+++ packages/git-testkit/src/git-testkit.js
@@ -0,0 +1,58 @@
+'use strict'
+const {promisify} = require('util')
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+const {exec} = require('child_process')
+const execAsync = promisify(exec)
+
+/**@type {string} */
+async function makeTemporaryDirectory() {
+  return await fs.promises.mkdtemp(os.tmpdir() + '/')
+}
+
+/**@type {Promise<undefined>} */
+async function init(/**@type {string} */ gitDir) {
+  await execAsync('git init', {cwd: gitDir})
+}
+
+/**@type {Promise<undefined>} */
+async function writeFile(
+  /**@type {string} */ gitDir,
+  /**@type {string} */ filePath,
+  /**@type {string|Buffer} */ content,
+) {
+  await fs.promises.writeFile(path.join(gitDir, filePath), content)
+}
+
+/**@type {Promise<undefined>} */
+async function writePackageJson(
+  /**@type {string} */ gitDir,
+  /**@type {string} */ filePath,
+  /**@type {string} */ name,
+  /**@type {string[]} */ ...dependencies
+) {
+  await fs.promises.mkdir(path.dirname(path.join(gitDir, filePath)), {recursive: true})
+  await fs.promises.writeFile(
+    path.join(gitDir, filePath),
+    JSON.stringify({
+      name,
+      version: '1.0.0',
+      dependencies: Object.fromEntries(dependencies.map((dep) => [dep, '^1.0.0'])),
+    }),
+  )
+}
+
+/**@type {Promise<undefined>} */
+async function commitAll(/**@type {string} */ gitDir, /**@type {string?} */ message = undefined) {
+  await execAsync('git add .', {cwd: gitDir})
+  await execAsync(`git commit -m "${message ? 'message' : 'nomessage'}"`, {cwd: gitDir})
+}
+
+module.exports = {
+  makeTemporaryDirectory,
+  init,
+  writeFile,
+  writePackageJson,
+  commitAll,
+}
