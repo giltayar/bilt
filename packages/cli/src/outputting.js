6b99ed780ea2c26daaaf0e9903a930b11343bdd6
Gil Tayar <gil@tayar.org>
2020-04-10 14:05:36 +0300

    cli: changing package outputting to put package in front
    [bilt-artifacts]

--- packages/cli/src/outputting.js
+++ packages/cli/src/outputting.js
@@ -12,14 +12,14 @@ function globalHeader(msg) {
  * @param {string} msg
  */
 function globalFooter(msg) {
-  console.error(chalk.green('* ' + msg))
+  console.error(chalk.green(`* ${msg}`))
 }
 
 /**
  * @param {string} msg
  */
 function globalOperation(msg) {
-  console.error(chalk.green('* ' + msg))
+  console.error(chalk.green(`* ${msg}`))
 }
 
 /**
@@ -27,7 +27,7 @@ function globalOperation(msg) {
  * @param {import('@bilt/types').PackageInfo} packageInfo
  */
 function packageHeader(msg, packageInfo) {
-  console.error(chalk.greenBright.underline('**** ' + msg + ': ' + packageInfo.directory))
+  console.error(chalk.greenBright.underline(`**** [${packageInfo.directory}] ${msg}`))
 }
 
 /**
@@ -35,7 +35,7 @@ function packageHeader(msg, packageInfo) {
  * @param {import('@bilt/types').PackageInfo} packageInfo
  */
 function packageFooter(msg, packageInfo) {
-  console.error(chalk.green('**** ' + msg + ': ' + packageInfo.directory))
+  console.error(chalk.green(`**** [${packageInfo.directory}] ${msg}`))
 }
 
 /**
@@ -44,7 +44,7 @@ function packageFooter(msg, packageInfo) {
  * @param {any} _error
  */
 function packageErrorFooter(msg, packageInfo, _error) {
-  console.error(chalk.redBright.underline('**** ' + msg + ': ' + packageInfo.directory))
+  console.error(chalk.redBright.underline(`**** [${packageInfo.directory}] ${msg}`))
 }
 
 /**
@@ -52,7 +52,7 @@ function packageErrorFooter(msg, packageInfo, _error) {
  * @param {import('@bilt/types').PackageInfo} packageInfo
  */
 function packageOperation(msg, packageInfo) {
-  console.error(chalk.grey.underline('****** ' + msg + ': ' + packageInfo.directory))
+  console.error(chalk.grey.underline(`****** [${packageInfo.directory}] ${msg}`))
 }
 
 module.exports = {
